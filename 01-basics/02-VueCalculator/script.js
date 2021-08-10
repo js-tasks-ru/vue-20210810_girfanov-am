458c7f13879c24a0d7b666e438d73fb7e4966a02
Гирфанов Артур <girfanov_am@medinfocenter.ru>
2021-08-10 22:13:10 +0500

    Solve calc

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,41 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      firstArgument: 0,
+      secondArgument: 0,
+      result: 0,
+      operator: '',
+    };
+  },
+  computed: {
+    sum() {
+      return parseInt(this.firstArgument) + parseInt(this.secondArgument);
+    },
+    subtract() {
+      return parseInt(this.firstArgument) - parseInt(this.secondArgument);
+    },
+    multiply() {
+      return parseInt(this.firstArgument) * parseInt(this.secondArgument);
+    },
+    divide() {
+      let divisionResult = this.firstArgument / this.secondArgument;
+      divisionResult = Number.isInteger(divisionResult) == true ? divisionResult : divisionResult.toFixed(2);
+      return divisionResult;
+    },
+  },
+  methods: {
+    onChange() {
+      if (this.operator === 'sum') {
+        this.result = this.sum;
+      } else if (this.operator === 'divide') {
+        this.result = this.divide;
+      } else if (this.operator === 'subtract') {
+        this.result = this.subtract;
+      } else if (this.operator === 'multiply') {
+        this.result = this.multiply;
+      }
+    },
+  },
+}).mount('#app');
