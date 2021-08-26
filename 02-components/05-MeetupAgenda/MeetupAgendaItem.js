d472d02c061025a6f18084a232ea5bd5a499c1bb
Гирфанов Артур <girfanov_am@medinfocenter.ru>
2021-08-27 00:41:04 +0500

    Final component task solved + minor code clean up

--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -26,14 +26,14 @@ export default defineComponent({
       </div>
       <div class="agenda-item__col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
       <div class="agenda-item__col">
-        <h3 v-if="agendaItem.title != null" class="agenda-item__title">{{ agendaItem.title }}</h3>
+        <h3 v-if="!agendaItem.title" class="agenda-item__title">{{ agendaItem.title }}</h3>
         <h3 v-else class="agenda-item__title">{{ defaultTitle }}</h3>
         <p v-if="agendaItem.type == 'talk'" class="agenda-item__talk">
           <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot">agendaItem.speaker</span>
           <span class="agenda-item__lang">{{ agendaItem.language }}</span>
         </p>
-        <p v-if="agendaItem.description != null">{{ agendaItem.description }}</p>
+        <p v-if="!agendaItem.description">{{ agendaItem.description }}</p>
       </div>
     </div>`,
 });
--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,7 +1,8 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import UiContainer from './UiContainer.js';
+import MeetupView from '../06-MeetupView/MeetupView.js';
+import { fetchMeetupById } from './meetupService.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,55 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+      curentMeetup: null,
+    };
+  },
+  watch: {
+    async meetupId() {
+      this.fetchMeetup();
+    },
+  },
+  async created() {
+    this.fetchMeetup();
+  },
+  methods: {
+    async fetchMeetup() {
+      this.isLoading = true;
+      this.curentMeetup = null;
+      this.error = null;
+      try {
+        this.curentMeetup = await fetchMeetupById(this.meetupId);
+        this.isLoading = false;
+      } catch (e) {
+        this.isLoading = false;
+        this.error = e.message;
+      }
+    },
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <meetup-view v-if="curentMeetup" :meetup="curentMeetup"></meetup-view>
 
-      <ui-container>
+      <ui-container v-else-if="isLoading">
         <ui-alert>Загрузка...</ui-alert>
       </ui-container>
 
-      <ui-container>
-        <ui-alert>error</ui-alert>
+      <ui-container v-else-if="error">
+        <ui-alert>{{ error }}</ui-alert>
       </ui-container>
     </div>`,
 });
